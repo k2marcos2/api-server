7ef4c8ed3710f58cd76fd873c5e45f5fc1dd8d6b
k2marcos2 <marcosfeitosapalmas2@gmail.com>
2025-04-03 11:27:00 -0300

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 
 const app = express();
 app.use(express.json());
 const users = []
 
-app.post('/users',(req, res) =>{
+app.post('/users',async(req, res) =>{
 
-    users.push(req.body);
-    res.status(201).json(users);
-})
-app.get('/users',(req, res) =>{
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body);
+});
+app.get('/users', async (req, res) =>{
+
+    const users = await prisma.user.findMany();
     res.status(200).json(users);
 });
 app.listen(8082);
