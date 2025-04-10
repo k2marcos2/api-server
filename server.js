2c541b014fb3d9fb05a85d4f94d57fa32b1dc058
k2marcos2 <marcosfeitosapalmas2@gmail.com>
2025-04-10 11:27:11 -0300

    compatibilidade dos servidores

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors'
 
 const prisma = new PrismaClient();
 
-
-const app = express();
+const app = express(); 
 app.use(express.json());
-const users = []
+app.use(cors('http://localhost:5173'));
 
-app.post('/users',async(req, res) =>{
-
+//Criar dados / ususarios / fazer requeisição POST
+app.post('/users', async (req, res) => {
     await prisma.user.create({
         data: {
             name: req.body.name,
@@ -18,12 +17,58 @@ app.post('/users',async(req, res) =>{
             age: req.body.age
         }
     })
-
     res.status(201).json(req.body);
-});
-app.get('/users', async (req, res) =>{
+})
 
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+//Mostrar /dados / usuarios / fazer requeisição GET
+
+app.get('/users', async (req, res) => {
+
+    let users = [];
+
+    if (req.query) { 
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+        return res.status(200).json(users);
+    }else{
+
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    }
 });
+
+
+//Deletar dados dados / ususarios / fazer requeisição PUT
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+
+    })
+    res.status(201).json({ messege: 'Usuario atualizado com sucesso!' });
+})
+
+//Deletar dados dados 44 / ususarios / fazer requeisição DELETE 
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+        
+    })
+    res.status(201).json({ messege: 'Usuario atualizado com sucesso!' });
+})
+
+//ip 127.0.0.1:8082
 app.listen(8082);
